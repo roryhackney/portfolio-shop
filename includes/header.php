4aae3e6094f5ee814031e751bdbfc5727e10a600
roryhackney <roryhackney@gmail.com>
2021-06-18 19:54:27 -0700

    favicons

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,15 @@
         <meta name="description" content=
         "Portfolio, shop, and blog for Rory, a trans neurodiverse artist who makes comics, games, and websites.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="./favicon.ico">
+        <!--favicons-->
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <!--fonts, javascript, and stylesheets-->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Mali&family=Seaweed+Script:wght@400;600&display=swap"
         rel="stylesheet">
